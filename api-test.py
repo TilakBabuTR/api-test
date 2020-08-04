9b7a3eafe258a09a6ad8b3764795a4e4726e21e2
TilakBabuTR <tilakbabu.bopparaju@thomsonreuters.com>
2020-08-04 20:05:38 +0530

    sys argv

--- api-test.py
+++ api-test.py
@@ -1,12 +1,11 @@
-import os
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
-username = 'vamsy.nallani@thomsonreuters.com'
-password = os.getenv('JenkinsAPIToken')
 
-print(password)
+f_n,username,password = sys.argv
+print("Got username", username)
+print("Got Password", password)
 
 url = 'http://10.114.4.99:8080/job/Barossa-Website-fullstack-UI-Regression-Entities/16/api/json'
 
